875e8d8950a023f0875a7f9516520814544652e0
Diogo Pernes <diogo.pernes.cunha@gmail.com>
2021-06-19 21:43:52 +0100

    adds solution of MT2 16/17

---
+++ PROG2/MT2_1617/prob1/prob1.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "avl.h"
+#include "grafo.h"
+
+/*** problema 1.1 ***/
+char *avl_maiorstring(no_avl *no)
+{
+    if (!no)
+        return NULL;
+
+    char *str_no = no->str;
+    char *str_esq = avl_maiorstring(no->esquerda); // procurar a maior string na sub-árvore da esquerda
+    char *str_dir = avl_maiorstring(no->direita);  // procurar a maior string na sub-árvore da direita
+
+    // calcular os tamanhos das 3 strings (string NULL tem tamanho 0)
+    int len_no = 0, len_esq = 0, len_dir = 0;
+    if (str_no != NULL)
+        len_no = strlen(str_no);
+    if (str_esq != NULL)
+        len_esq = strlen(str_esq);
+    if (str_dir != NULL)
+        len_dir = strlen(str_dir);
+
+    // comparar os tamanhos para determinar qual a maior das três strings
+    int max_len = len_no;
+    char *max_str = str_no;
+    if (len_esq > max_len)
+    {
+        max_len = len_esq;
+        max_str = str_esq;
+    }
+    if (len_dir > max_len)
+    {
+        max_len = len_dir;
+        max_str = str_dir;
+    }
+
+    return max_str;
+}
+
+/*** problema 1.2 ***/
+
+/*
+Solução 1 (mais simples, menos eficiente):
+    Utilizar a função grafo_bfs para determinar a distância entre a origem e cada um dos restantes nós.
+    Guardar as distâncias entre a origem e cada um dos nós num vetor.
+    Procurar o máximo do vetor de distâncias.
+
+Solução 2 (menos óbvia, mais eficiente): ver código abaixo.
+*/
+
+/* função auxiliar de grafo_maisdistante (chamada recursivamente) */
+int grafo_maisdistante_rec(grafo *g, int origem, int *distancia, int *visitados)
+{
+    visitados[origem] = 1; // registar o nó atual como visitado
+    int max_dist = -1, max_vertice = origem;
+
+    // percorrer os filhos do nó atual e procurar aquele que retorna a maior distância
+    for (lista_no *aresta = g->adjacencias[origem].inicio; aresta != NULL; aresta = aresta->proximo)
+    {
+        if (visitados[aresta->vertice]) // se já visitámos este nó, este caminho não é mínimo
+            continue;
+
+        int dist, vertice = grafo_maisdistante_rec(g, aresta->vertice, &dist, visitados);
+        if (dist > max_dist)
+        {
+            max_dist = dist;
+            max_vertice = vertice;
+        }
+    }
+
+    // a distância retornada ao nó-pai é 1 + a maior distância retornada pelos nós-filhos
+    *distancia = 1 + max_dist;
+    return max_vertice;
+}
+
+int grafo_maisdistante(grafo *g, int origem, int *distancia)
+{
+    if ((g == NULL) || (origem < 0) || (origem > g->tamanho - 1) || (distancia == NULL))
+        return -1;
+
+    // utilizar array auxiliar para registar que nós já foram visitados
+    int visitados[g->tamanho];
+    for (int i = 0; i < g->tamanho; i++)
+        visitados[i] = 0;
+
+    // chamar a função auxiliar recursivamente a partir do nó origem
+    return grafo_maisdistante_rec(g, origem, distancia, visitados);
+}
+
+/* Aqui comeca o codigo de teste. Nao modificar! */
+
+#define MAX_STR 500
+
+int main()
+{
+    setvbuf(stdout, NULL, _IONBF, 0);
+    FILE *fp = fopen("paises.txt", "r");
+    char str_aux[MAX_STR], *str;
+
+    /* teste problema 1.1 */
+    {
+        arvore_avl *arv;
+        puts("* Problema 1.1 *");
+        arv = avl_nova();
+        while (fgets(str_aux, MAX_STR, fp) != NULL)
+        {
+            if (str_aux[strlen(str_aux) - 1] == '\n')
+                str_aux[strlen(str_aux) - 1] = '\0';
+            avl_insere(arv, str_aux);
+        }
+        str = avl_maiorstring(arv->raiz);
+        printf("Maior string: ");
+        if (str != NULL)
+            puts(str);
+        else
+            puts("(null)");
+        avl_apaga(arv);
+    }
+
+    /* teste problema 1.2 */
+    {
+        grafo *g;
+        int vertice, comprimento;
+        puts("\n* Problema 1.2 *");
+        g = grafo_novo(8, DIGRAFO);
+        grafo_adiciona(g, 0, 4);
+        grafo_adiciona(g, 1, 0);
+        grafo_adiciona(g, 1, 5);
+        grafo_adiciona(g, 2, 1);
+        grafo_adiciona(g, 3, 2);
+        grafo_adiciona(g, 4, 3);
+        grafo_adiciona(g, 5, 4);
+        grafo_adiciona(g, 6, 3);
+        grafo_adiciona(g, 7, 2);
+        grafo_adiciona(g, 7, 6);
+
+        vertice = grafo_maisdistante(g, 1, &comprimento);
+        if (vertice != -1)
+            printf("Mais distante do vertice 1: %d (distancia %d)\n", vertice, comprimento);
+        vertice = grafo_maisdistante(g, 6, &comprimento);
+        if (vertice != -1)
+            printf("Mais distante do vertice 6: %d (distancia %d)\n", vertice, comprimento);
+
+        grafo_apaga(g);
+    }
+
+    fclose(fp);
+    return 0;
+}
---
+++ PROG2/MT2_1617/prob2/prob2.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vetor.h"
+#include "heap.h"
+
+/*** problema 2.1 ***/
+vetor *heap_ordena(vetor *v)
+{
+    if (v == NULL)
+        return NULL;
+
+    heap *h = heap_nova(v->tamanho);
+    if (h == NULL)
+        return NULL;
+
+    for (int i = 0; i < v->tamanho; i++)
+    {
+        char *str = v->elementos[i].str;
+        int prioridade = (str[0] << 8) + str[1];
+        if( heap_insere(h, str, prioridade) == 0)
+        {
+            heap_apaga(h);
+            return NULL;
+        }
+    }
+
+    vetor *v_ord = vetor_novo();
+    if(v_ord == NULL)
+    {
+        heap_apaga(h);
+        return NULL;
+    }
+
+    while(h->tamanho > 0)
+    {
+        char *str = heap_remove(h);
+        if (vetor_insere(v_ord, str, -1) == -1)
+        {
+            vetor_apaga(v_ord);
+            heap_apaga(h);
+            return NULL;
+        }
+    }
+    heap_apaga(h);
+
+    for(int i = 0; i < v_ord->tamanho / 2; i++)
+    {
+        v_elemento aux = v_ord->elementos[i];
+        v_ord->elementos[i] = v_ord->elementos[v_ord->tamanho - i - 1];
+        v_ord->elementos[v_ord->tamanho - i - 1] = aux;
+    }
+
+    return v_ord;
+}
+
+/*** problema 2.2 ***/
+/*
+complexidade do algoritmo:
+O(n*log(n)), onde n é o tamanho do vetor
+
+justificacao:
+n operações de inserção na heap: n*O(log(n)) = O(n*log(n))
+n operações de remoção na heap: n*O(log(n)) = O(n*log(n))
+n operações de inserção no final do vetor: n*O(1) = O(n)
+inversão do vetor: O(n)
+total: O(n*log(n)) + O(n*log(n)) + O(n) + O(n) = O(n*log(n))
+*/
+
+/* Aqui comeca o codigo de teste. Nao modificar! */
+
+#define MAX_LINE 100
+
+int main()
+{
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    /* teste problema 2.1 */
+    {
+        int i, j;
+        char cidade[MAX_LINE];
+        vetor *v, *res;
+        FILE *fp;
+
+        puts("* Problema 2.1 *");
+        v = vetor_novo();
+        fp = fopen("cidades.txt", "r");
+        while (fgets(cidade, MAX_LINE, fp) != NULL)
+        {
+            *strchr(cidade, ',') = '\0';
+            vetor_insere(v, cidade, v->tamanho);
+        }
+        res = heap_ordena(v);
+        if (res != NULL)
+        {
+            for (i = 0; i < 3 && i < vetor_tamanho(res); i++)
+                printf("%s\n", vetor_elemento(res, i));
+            if (vetor_tamanho(res) > 3)
+            {
+                if (vetor_tamanho(res) > 6)
+                    puts("...");
+                j = i;
+                if (vetor_tamanho(res) - 3 > j)
+                    j = vetor_tamanho(res) - 3;
+                for (i = j; i < vetor_tamanho(res); i++)
+                    printf("%s\n", vetor_elemento(res, i));
+            }
+        }
+
+        vetor_apaga(v);
+        vetor_apaga(res);
+    }
+
+    return 0;
+}
